c25b7d24ff14d7921190cdb899d6f33cfeca5448
Aldo Escobar <aoelvp94@gmail.com>
2024-02-23 12:47:34 -0300

    add superset

---
+++ docker/entrypoints/start_superset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script that allows the user and connection creation for Superset service. Also it would allow us to import dashboards/datasources.
+if [ "$SUPERSET_CREATE_USER" = true ]; then
+    echo "Creating superset user..."
+    superset-init --username "$SUPERSET_USER" --password "$SUPERSET_PASSWORD" --firstname "$SUPERSET_FIRSTNAME" --lastname "$SUPERSET_LASTNAME" --email "$SUPERSET_EMAIL"
+    pip3 install duckdb-engine
+    cp ../dbt_project/data.duckdb ../superset/data.duckdb
+    superset set_database_uri -d duckdb -u duckdb:///home/superset/db/data.duckdb
+fi
+
+gunicorn --bind 0.0.0.0:"$SUPERSET_PORT" -w 10 --timeout 120 "superset.app:create_app()"
